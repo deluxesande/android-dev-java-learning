01c991c9613318bc9b3ba844ba16d6d1f5024c75
deluxesande <deluxesande@gmail.com>
2024-10-22 15:13:14 +0300

    Input and fetching data in java UI and displaying it.

--- app/src/main/java/me/deluxesande/helloworld/MainActivity.java
+++ app/src/main/java/me/deluxesande/helloworld/MainActivity.java
@@ -2,6 +2,8 @@ package me.deluxesande.helloworld;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText firstNameInput;
+    private EditText lastNameInput;
+    private EditText emailInput;
+
+    private TextView firstNameOutput;
+    private TextView lastNameOutput;
+    private TextView emailOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +31,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        firstNameInput = findViewById(R.id.firstNameInput);
+        lastNameInput = findViewById(R.id.lastNameInput);
+        emailInput = findViewById(R.id.emailInput);
+
+        firstNameOutput = findViewById(R.id.firstNameOutput);
+        lastNameOutput = findViewById(R.id.lastNameOutput);
+        emailOutput = findViewById(R.id.emailOutput);
+    }
+
+    public void processInputData() {
+        // Getting Values from the Input Field
+        String firstName = firstNameInput.getText().toString();
+        String lastName = lastNameInput.getText().toString();
+        String email = emailInput.getText().toString();
+
+        // Setting the Output Field
+        firstNameOutput.setText(firstName);
+        lastNameOutput.setText(lastName);
+        emailOutput.setText(email);
     }
 
     public void onButtonClick(View view) {
-
+        processInputData();
     }
 }
